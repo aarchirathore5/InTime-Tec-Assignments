5179e145e4fa8a1c8829abc005bcd620bd598c57
aarchirathore5 <168953714+aarchirathore5@users.noreply.github.com>
2025-11-07 11:36:25 +0530

    Refactor vfs.h for clarity and structure

--- Assignment 6/vfs.h
+++ Assignment 6/vfs.h
@@ -6,35 +6,27 @@
 #define BLOCK_SIZE 512
 #define MAX_NAME_LEN 50
 
-// free block node, doubly linked
 typedef struct FreeBlock {
     int blockIndex;
     struct FreeBlock* next;
     struct FreeBlock* prev;
 } FreeBlock;
 
-// file or directory node
 typedef struct FileNode {
     char name[MAX_NAME_LEN + 1];
-    int isDirectory;           // 1 => directory, 0 => file
+    int isDirectory;
     struct FileNode* parent;
-
-    // for directories: circular doubly-linked children
     struct FileNode* childHead;
     struct FileNode* next;
     struct FileNode* prev;
-
-    // for files: blocks and size
     int* blockPointers;
     size_t blockCount;
     size_t contentSize;
 } FileNode;
 
-// init/shutdown
-void vfsInitialize(int numBlocks);   // To start vfs
-void vfsShutdown(void);              // To clean everything up
+void vfsInitialize(int numBlocks);
+void vfsShutdown(void);
 
-// commands (used by CLI)
 void cmdMkdir(const char* dirname);
 void cmdCreate(const char* filename);
 void cmdCd(const char* dirname);
@@ -46,7 +38,8 @@ void cmdDelete(const char* filename);
 void cmdRead(const char* filename);
 void cmdWrite(const char* filename, const char* content);
 
-// To help for prompt
 FileNode* vfsGetCwd(void);
 
-#endif // VFS_H
+void runCli(void);
+
+#endif
